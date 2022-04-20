c32fad27bc9b4c523af574ff2c41c1e018971174
ayman-erroutabi <nasura789@gmail.com>
2022-04-20 01:31:28 +0000

    initial app

---
+++ src/main/java/ma/rightman/openapidemo/OpenapidemoApplication.java
@@ -0,0 +1,13 @@
+package ma.rightman.openapidemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenapidemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenapidemoApplication.class, args);
+	}
+
+}
