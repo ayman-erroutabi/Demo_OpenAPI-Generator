da4af54de2deda0156b617c04169a22deeab9d41
ayman-erroutabi <nasura789@gmail.com>
2022-05-01 00:33:23 +0000

    Add Students CRUD
    + Add liquibase configuration
    + Implementation of OpenAPI auto generated API (server stub)
    + Insertion of dump data through liquibase

---
+++ src/main/java/ma/rightman/openapidemo/services/StudentService.java
@@ -0,0 +1,8 @@
+package ma.rightman.openapidemo.services;
+
+import ma.rightman.openapidemo.api.StudentsApiDelegate;
+import org.springframework.stereotype.Service;
+
+@Service
+public interface StudentService extends StudentsApiDelegate {
+}
